93b16fd5788c24dd94db4e7d6882a8dd4ab95bfd
Jarlyyn <jarlyyn@qq.com>
2023-12-28 00:40:17 +0800

    init

---
+++ script/main.js
@@ -0,0 +1,29 @@
+onOpen=function (){
+
+}
+
+onClose=function (){
+
+}
+
+onConnected=function (){
+
+}
+
+onDisconnected=function (){
+
+}
+
+onAssist=function(){
+
+}
+
+onBroadcast=function(msg,global,channel,global){
+    
+}
+onResponse=function(type,id,data){
+    
+}
+onkeyup=function(key){
+
+}
